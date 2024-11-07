5467f193ad8a20ac9b79ef3a261a192734f85bf5
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-08 00:15:46 +0530

    Background Added

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] Transform _playerREF;
     [SerializeField] private float _cameraFollowSpeed;
-    [SerializeField] private Vector3 offSet;
 
     private void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(_playerREF.position.x + offSet.x, _playerREF.position.y + offSet.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, _cameraFollowSpeed * Time.deltaTime);
+        transform.position += new Vector3(_cameraFollowSpeed * Time.deltaTime, 0, 0);
     }
 }
