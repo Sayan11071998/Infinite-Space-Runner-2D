f466119d09bd0168316fc60ec9b2a82b9c966395
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-08 16:54:25 +0530

    Final Polishing

--- Assets/Scripts/UI/GameOverUIController.cs
+++ Assets/Scripts/UI/GameOverUIController.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverUIController : MonoBehaviour
 {
     [SerializeField] GameObject _gameOverPanel;
+    [SerializeField] private Button _restartGameButton;
+    [SerializeField] private Button _mainMenuButton;
 
     private void Awake()
     {
         _gameOverPanel.SetActive(false);
+
+        _restartGameButton.onClick.AddListener(RestartGame);
+        _mainMenuButton.onClick.AddListener(MainMenu);
     }
 
     public void GameOver()
     {
-        _gameOverPanel.SetActive(true);
+        if (_gameOverPanel != null)
+            _gameOverPanel.SetActive(true);
     }
 
     public void RestartGame()
@@ -22,5 +29,10 @@ public class GameOverUIController : MonoBehaviour
         GameManager.Instance._isGameOver = false;
         AudioManager.Instance.PlaySFX(AudioTypeList.ButtonSound);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
