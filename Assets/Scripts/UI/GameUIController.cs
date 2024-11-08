2698869559585d437a00163610f5c121a3832e47
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-08 16:12:10 +0530

    Final Polishing

--- Assets/Scripts/UI/GameUIController.cs
+++ Assets/Scripts/UI/GameUIController.cs
@@ -8,6 +8,12 @@ public class GameUIController : MonoBehaviour
     [SerializeField] private TMP_Text _healthText;
     [SerializeField] private TMP_Text _scoreText;
 
+    private void Update()
+    {
+        if (GameManager.Instance._isGameOver)
+            gameObject.SetActive(false);
+    }
+
     public void UpdateHealth(float _healAmount)
     {
         _healthText.text = "Health: " + _healAmount.ToString("0");
