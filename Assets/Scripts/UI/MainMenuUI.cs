0fd34b68301c8912dd277ab1855e410fa46b7a0a
Sayan Nandi <sayannandi11071998@gmail.com>
2024-11-08 16:47:46 +0530

    Final Polishing

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _quitButton;
+
+    private void Awake()
+    {
+        _startGameButton.onClick.AddListener(StartGame);
+        _quitButton.onClick.AddListener(Quit);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
